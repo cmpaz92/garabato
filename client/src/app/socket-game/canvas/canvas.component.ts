38d4fb5ffc3fe3067a00d7df6cd236102ae7a668
Maukimann <Sporn.Maurice@gmail.com>
2020-05-25 10:58:34 +0200

    Added game logic

--- client/src/app/socket-game/canvas/canvas.component.ts
+++ client/src/app/socket-game/canvas/canvas.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-canvas',
@@ -9,78 +10,47 @@ export class CanvasComponent implements OnInit {
   @Input() socket: any;
   @ViewChild('game')
   private gameCanvas: ElementRef;
-  public context: any;
-  public players: any[] = [];
-  public player: any;
-  public id: Number;
+  private context: any;
+  private roomID: number;
+
+  private rect: any;
+  private scaleX: number;
+  private scaleY: number;
+  private isDrawing: boolean = false;
+  private drawingEnabled:boolean = false;
 
   public ngOnInit() {
     console.log(this.socket);
+    this.roomID = 0; // TODO how do we want to set the room ID? On creation or have a set number of rooms?
   }
+
   public ngAfterViewInit() {
     this.context = this.gameCanvas.nativeElement.getContext('2d');
+    this.rect = this.gameCanvas.nativeElement.getBoundingClientRect();
+    this.scaleX = this.gameCanvas.nativeElement.width / this.rect.width, 
+    this.scaleY = this.gameCanvas.nativeElement.height / this.rect.height; 
 
-    this.socket.on('heartbeat', (data) => {
-      this.players = data;
+    this.enableDrawing().subscribe();
+    this.updateCanvas().subscribe();
+    this.clearCanvas().subscribe();
 
-      console.log('Client user: ' + this.player);
-      console.log('All players: ' + this.players);
-
-      for (let i = this.players.length - 1; i >= 0; i--) {
-        if (this.players[i].id !== this.socket.id) {
-          if (this.player.room == this.players[i].room) {
-            this.context.fillStyle = 'red';
-            this.context.fillRect(this.players[i].x, this.players[i].y, 20, 20);
-            this.context.font = '10px Georgia';
-            this.context.fillText(
-              this.players[i].id,
-              this.players[i].x,
-              this.players[i].y
-            );
-          }
-        }
+    document.onmousedown = (e) => {
+      if (!!this.drawingEnabled) {
+        // only draw, when it is your turn to draw
+        this.isDrawing = true;
       }
-    });
+    };
 
-    this.socket.on('createPlayer', (data: any) => {
-      for (let i = data.length - 1; i >= 0; i--) {
-        if (data[i].id == this.socket.id) {
-          this.player = data[i];
-          console.log(this.player);
-          this.context.fillStyle = 'white';
-          this.context.fillText(data[i].user, data[i].x, data[i].y);
-          this.context.fillStyle = 'white';
-          this.context.fillRect(data[i].x, data[i].y, 20, 20);
-        }
+    document.onmouseup = (e) => {
+      this.isDrawing = false;
+    };
+
+    document.onmousemove = (e) => {
+      if (this.isDrawing){
+        // event get mouse position in world space, but drawing is done in local space
+        // therefore the the position first needs to be mapped in local space
+        this.drawRequest(((e.clientX - this.rect.left) * this.scaleX), ((e.clientY - this.rect.top)* this.scaleY));
       }
-    });
-
-    this.socket.on('deletePlayer', (data: any) => {
-      for (let i = data.length - 1; i >= 0; i--) {
-        if (data[i].id == this.socket.id) {
-          this.context.clearRect(data[i].x, data[i].y, 20, 20);
-        }
-      }
-    });
-
-    this.socket.on('showNewPlayer', (data: any) => {
-      for (let i = data.length - 1; i >= 0; i--) {
-        if (data[i].id !== this.socket.id) {
-          this.context.fillStyle = 'white';
-          this.context.fillText(data[i].user, data[i].x, data[i].y);
-          this.context.fillStyle = 'white';
-          this.context.fillRect(data[i].x, data[i].y, 20, 20);
-        }
-      }
-    });
-
-    document.onkeydown = (e) => {
-      let dir: string;
-      if (e.keyCode === 39) dir = 'right';
-      if (e.keyCode === 37) dir = 'left';
-      if (e.keyCode === 38) dir = 'up';
-      if (e.keyCode === 40) dir = 'down';
-      this.send(dir, this.player);
     };
 
     this.socket.on('init', (data) => {
@@ -88,40 +58,61 @@ export class CanvasComponent implements OnInit {
     });
   }
 
-  position(data) {
-    this.context.clearRect(
-      0,
-      0,
-      this.gameCanvas.nativeElement.width,
-      this.gameCanvas.nativeElement.height
+  drawRequest(xPos: number, yPos: number) {
+    // send draw request to server
+    this.socket.emit('draw', xPos, yPos, this.roomID);
+  }
+
+  enableDrawing() {
+    let observable = new Observable(
+      (observer) => {
+        this.socket.on('enableDrawing', (enable:boolean) => {
+          this.drawingEnabled = enable;
+          observer.next(enable);
+        });
+        return () => {
+          this.socket.disconnect();
+        };
+      }
     );
-
-    this.context.fillStyle = 'white';
-    this.context.fillText(data.user, data.x, data.y);
-    this.context.fillStyle = 'white';
-    this.context.fillRect(data.x, data.y, 20, 20);
+    return observable;
   }
 
-  send(dir: String, data: any) {
-    if (dir === 'left') {
-      data.x = data.x - 20;
-      this.position(data);
-      this.socket.emit('move', data);
-    }
-    if (dir === 'right') {
-      data.x = data.x + 20;
-      this.position(data);
-      this.socket.emit('move', data);
-    }
-    if (dir === 'up') {
-      data.y = data.y - 20;
-      this.position(data);
-      this.socket.emit('move', data);
-    }
-    if (dir === 'down') {
-      data.y = data.y + 20;
-      this.position(data);
-      this.socket.emit('move', data);
-    }
+  clearCanvas() {
+    let observable = new Observable(
+      (observer) => {
+        this.socket.on('clearCanvas', () => {
+          this.context.clearRect(
+            0,
+            0,
+            this.gameCanvas.nativeElement.width,
+            this.gameCanvas.nativeElement.height
+          );
+          observer.next();
+        });
+        return () => {
+          this.socket.disconnect();
+        };
+      }
+    );
+    return observable;
   }
+
+  updateCanvas() {
+    let observable = new Observable<{ posX: number; posY: number }>(
+      (observer) => {
+        this.socket.on('update canvas', (pos) => {
+          this.context.fillStyle = 'white';
+          this.context.fillRect(pos.posX, pos.posY, 5, 5);
+          // console.log("Currently drawing on:  posX: " + pos.posX + "| posY" + pos.posY)
+          observer.next(pos);
+        });
+        return () => {
+          this.socket.disconnect();
+        };
+      }
+    );
+    return observable;
+  }
+
 }
