b19ba291292fd968a5fafab10ef5ca2e94082d8f
cmpaz92 <cmp0592@gmail.com>
2020-05-27 20:56:54 +0200

    Cors policy

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const Express = require("express")();
 const Http = require("http").Server(Express);
 const Socketio = require("socket.io")(Http, { origins: '*:*'});
-Socketio.origins('*:*');
+
 
 var words = require('./data.json');
 let rooms = new Map();
