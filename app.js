beadba99fe1747a93a95125f0e203bc5f818e40d
Maukimann <Sporn.Maurice@gmail.com>
2020-05-27 10:37:59 +0200

    Added features, fixed stuff

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ Http.listen(3000, () => {
   console.log("Server is running");
 });
 
+const ROUND_TIME = 30;
+
 function user(id, userName) {
   this.id = id;
   this.userName = userName;
@@ -15,7 +17,8 @@ function roomData (players) {
   this.players = players;
   this.peopleGuessing = 0;
   this.currentWord = "";
-  this.timeLeft = -1; // TODO has no function yet
+  this.countdownTime = 30; 
+  this.timeIntervalBuffer; // the actual counter
 }
 
 function findPlayerByName (room, name) {
@@ -26,7 +29,6 @@ function findPlayerByName (room, name) {
       var nameBuffer2 = players[i].userName; // use buffers, because the string comparison fails otherwise 
       if (nameBuffer.localeCompare(nameBuffer2) == 0){
         // find the player by userName
-        // console.log("userName: " + userName + " = players[i].userName: " + players[i].userName);
         playerIndex = i;
         break;
       }
@@ -48,6 +50,7 @@ function startGame (room) {
   console.log("Selected Player to draw: " + players[playerIndex].userName);
 
   Socketio.in(room).emit("clearCanvas"); // new drawing -> clear canvas for everybody in the room
+  startCountdownTimer(ROUND_TIME, room);
   for (let i = 0; i < players.length; i++){
     // get random player
     if (i == playerIndex){
@@ -58,6 +61,7 @@ function startGame (room) {
         message: "Your word is: " + words[wordIndex],
       });
       Socketio.to(players[i].id).emit("enableDrawing", true);
+      Socketio.to(players[i].id).emit("setWordToDraw", words[wordIndex]);
     } else {
       players[i].stillGuessing = true; // these players have to guess
       Socketio.to(players[i].id).emit("new message", {
@@ -65,6 +69,7 @@ function startGame (room) {
         message: "It is your time to guess!",
       });
       Socketio.to(players[i].id).emit("enableDrawing", false);
+      Socketio.to(players[i].id).emit("setWordToDraw", ""); // clear the word so players don't get confused
     }
   }
 }
@@ -74,21 +79,44 @@ function checkIfRoundIsOver(room) {
     // round is over -> broadcast to players
     Socketio.in(room).emit("new message", {
       user: "Server",
-      message: "Round is over!",
+      message: "Round is over! The correct word was: " +  rooms.get(room).currentWord,
     });
     // reset values
-    rooms.get(room).timeLeft = -1; // reset time; TODO time has no function yet
+    clearInterval(rooms.get(room).timeIntervalBuffer); // stop timer
+    rooms.get(room).countdownTime = 30; // reset time
     rooms.get(room).currentWord = ""; // reset word
   }
 }
 
-let words = ["apple", "bird", "city", "anger", "shower"]; // TODO add words, maybe read that stuff from another file
+function startCountdownTimer(seconds, room) {
+  rooms.get(room).countdownTime = seconds;
+
+  clearInterval(rooms.get(room).timeIntervalBuffer); // clear the old interval before starting a new one  
+
+  rooms.get(room).timeIntervalBuffer = setInterval(() => {
+    Socketio.in(room).emit("setTimer", rooms.get(room).countdownTime); // send current time
+    rooms.get(room).countdownTime--;
+
+    if (rooms.get(room).countdownTime < 0 ) {
+      // time is over -> clear interval
+      clearInterval(rooms.get(room).timeIntervalBuffer); 
+      Socketio.in(room).emit("new message", {
+        user: "Server",
+        message: "Round is over! The correct word was: " +  rooms.get(room).currentWord,
+      });
+      Socketio.in(room).emit("setWordToDraw", rooms.get(room).currentWord); // update the view of all players
+      rooms.get(room).currentWord = ""; // reset word
+    }
+  }, 1000);
+}
+
+let words = ["apple", "bird", "city", "anger", "shower", "car"]; // TODO add words, maybe read that stuff from another file
 let rooms = new Map();
 
 Socketio.on("connection", (socket) => {
   socket.on("newPlayer", (userName, room) => {
     socket.join(room);
-
+    console.log("Test I am here");
     player = new user(socket.id, userName);
     if (rooms.has(room)){
       // room already exists -> add player
@@ -126,6 +154,10 @@ Socketio.on("connection", (socket) => {
     });
   });
 
+  socket.on("clearCanvasRequest", (room) => {
+    Socketio.in(room).emit("clearCanvas"); // new drawing -> clear canvas for everybody in the room
+  });
+
   socket.on("join", (userName, room) => {
     //socket.join(room); // already joining on new Player TODO decide which one is used 
     console.log(userName + " joined the room " + room);
@@ -158,14 +190,20 @@ Socketio.on("connection", (socket) => {
   socket.on("message", (userName, room, messageText) => {
     // check if the message contains the right word
     if (rooms.get(room).currentWord != "" && messageText.includes((rooms.get(room).currentWord))){
-      Socketio.in(room).emit("new message", {
-        user: "Server",
-        message: userName + " guessed the correct word!",
-      });
       var playerIndex = findPlayerByName(room, userName);
-      rooms.get(room).players[playerIndex].stillGuessing = false;
-      rooms.get(room).peopleGuessing--;
-      checkIfRoundIsOver(room);
+      if (rooms.get(room).players[playerIndex].stillGuessing) {
+        // player is still guessing and finally got the correct word
+        Socketio.in(room).emit("new message", {
+          user: "Server",
+          message: userName + " guessed the correct word!",
+        });
+        Socketio.to(rooms.get(room).players[playerIndex].id).emit("setWordToDraw", rooms.get(room).currentWord); // update the view of the player
+        rooms.get(room).players[playerIndex].stillGuessing = false;
+        rooms.get(room).peopleGuessing--;
+        checkIfRoundIsOver(room);
+      }
+      // else branch would mean that the player already guessed the correct word and typed it again
+      // so don't the message again and dont reduce peopleGuessing
     } else {
       Socketio.in(room).emit("new message", {
         user: userName,
