c46be4395a6c953c5251d41eff72d6c4f2db5e5a
cmpaz92 <cmp0592@gmail.com>
2020-05-27 23:29:58 +0200

    cors policy

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 const Express = require("express")();
 const Http = require("http").Server(Express);
-
-const Socketio = require("socket.io")(Http, {
-  handlePreflightRequest: (req, res) => {
-      const headers = {
-          "Access-Control-Allow-Headers": "Content-Type, Authorization",
-          "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
-          "Access-Control-Allow-Credentials": true
-      };
-      res.writeHead(200, headers);
-      res.end();
-  }
+const Socketio = require("socket.io")(Http);
+Socketio.origins((origin, callback) => {
+ /* if (origin !== 'https://foo.example.com') {
+      return callback('origin not allowed', false);
+  }*/
+  callback(null, true);
 });
 
 var words = require('./data.json');
